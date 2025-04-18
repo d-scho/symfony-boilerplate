ab35998a086b7c0df53011815d6a1cec20a037f5
David Schönhofen <d.schoenhofen.dev@gmail.com>
2025-04-18 22:47:04 +0200

    Add command

---
+++ src/TestCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:test', description: 'This is a test command')]
+final class TestCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->addArgument('input', InputArgument::REQUIRED, 'Any input');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln(sprintf('Input: "%s"', $input->getArgument('input')));
+
+        return Command::SUCCESS;
+    }
+}
