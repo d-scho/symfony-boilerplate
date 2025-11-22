873c475d2539b9ee7c78cb6cf27eb3f446a7fc07
David Schönhofen <d.schoenhofen.dev@gmail.com>
2025-11-22 21:15:52 +0100

    Add preload.php

---
+++ config/preload.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Supported since 7.4 - file has to be added to php.ini
+ * @see https://symfony.com/blog/php-preloading-and-symfony
+ */
+if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
+}
